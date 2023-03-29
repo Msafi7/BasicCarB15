f69d3b744e13911b19f286067631093c7308441e
Munir Safi <munir.safi87@gmail.com>
2023-03-29 18:18:21 -0500

    the basic structure of the car is ready

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        BasicStructure audi=new BasicStructure();
+        audi.color="black";
+        audi.model="black";
+        audi.power="black";
+
+    }
+}
